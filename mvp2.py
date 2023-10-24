cf3caff9657c4c53af1e8f5b4734b7529cd2d199
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-10-23 23:40:33 -0400

    Update mvp2.py

--- mvp2.py
+++ mvp2.py
@@ -486,7 +486,6 @@ def main():
         braden_score_for_ulcer_patient_counts(ulcer_b)
         location_counts(ulcer_b)
         heal_rate_braden_score(brad,ulcer)
-        st.write(result.head(10))
         result = heal_logic(result)
         # Dist_Cate_Labels(result)
     st.markdown("Appendix: [The logic of graphs and analysis for reference]"
