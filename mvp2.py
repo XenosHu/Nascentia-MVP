f5b6a3d2703c9ff036d1dbce65b40c224801cbaa
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-10-23 21:00:13 -0400

    Update mvp2.py

--- mvp2.py
+++ mvp2.py
@@ -333,7 +333,7 @@ def heal_logic(result):
     result['last_assessment_score'] = result['Sorted_AssessmentAnswers'].apply(lambda x: x[-1])
     
     # Print the updated data with categorization for each row
-    return result
+    #return result
 
 #------------------------------------------------------------------------------------------------------
 def Dist_Cate_Labels(result):
@@ -485,7 +485,7 @@ def main():
         braden_score_for_ulcer_patient_counts(ulcer_b)
         location_counts(ulcer_b)
         heal_rate_braden_score(ulcer_b)
-        heal_logic(result)
+        result = heal_logic(result)
         Dist_Cate_Labels(result)
     st.markdown("Appendix: [The logic of graphs and analysis for reference]"
             "(https://drive.google.com/file/d/1gyZnA_mfkNlwyOyjKlLGgIH7LiEUQvZQ/view?usp=share_link)")
