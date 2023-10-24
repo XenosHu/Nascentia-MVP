2e19fef035e0c06aefed6fd2cf42da7b2a1afbe0
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-10-24 16:51:07 -0400

    Update mvp2.py

--- mvp2.py
+++ mvp2.py
@@ -520,6 +520,9 @@ def main():
         heal_rate_braden_score(brad,df3)
         result = heal_rate_braden_score(brad, ulcer)
         result = heal_logic(result)
+        st.write('df3:')
+        st.write(df3)
+        st.write('result:')
         st.write(result)
         Cate_given_brad(result)
     st.markdown("Appendix: [The logic of graphs and analysis for reference]"
