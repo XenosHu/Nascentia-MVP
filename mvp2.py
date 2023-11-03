6be1f1dd1a397dfab921c664519756ebafd93724
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-02 20:10:08 -0400

    Update mvp2.py

--- mvp2.py
+++ mvp2.py
@@ -164,12 +164,13 @@ def plot_ulcer_counts(ulcer):
 
 def plot_ulcer_counts_by_month(ulcer):
 
-    unique_ulcer_patients = ulcer.sort_values('SOE', ascending=False).drop_duplicates('Name')
+    unique_ulcer_patients = ulcer.sort_values('SOE', ascending=False).drop_duplicates('Name',keep = 'first')
     unique_ulcer_patients['Month'] = unique_ulcer_patients['SOE'].dt.to_period('M').dt.strftime('%b %Y')
+    month_order = unique_ulcer_patients['Month'].cat.as_ordered().unique()
+
     # Plot bar chart for Pressure Ulcer Count by Type and sorted by month
     type_counts_by_month = pd.crosstab(unique_ulcer_patients['Month'], unique_ulcer_patients['Type']).fillna(0)
     type_counts_by_month = type_counts_by_month.div(type_counts_by_month.sum(axis=1), axis=0) * 100
-    type_counts_by_month = type_counts_by_month.loc[month_order]
 
     st.write(type_counts_by_month)
     # plt.figure(figsize=(12, 8))
